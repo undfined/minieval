725dcd5042c958d42aee1fff695c1b63ec6be940
Tyler Murray <tylerm@allenai.org>
2026-01-22 12:45:16 -0800

    Just use same vllm

--- minieval/launchers/beaker/launcher.py
+++ minieval/launchers/beaker/launcher.py
@@ -147,7 +147,7 @@ def launch_gantry(config: BeakerConfig):
     env_vars = [f"{var.name}={var.value}" for var in config.env_vars]
     env_secrets = [f"{var.name}={var.secret}" for var in config.env_secrets if var.secret is not None]
 
-    INSTALL_CMD = "uv pip uninstall transformers && uv pip install -e '.[all]'"
+    INSTALL_CMD = "uv pip install -e '.[all]'"
 
     # Launch the experiment
     try:
